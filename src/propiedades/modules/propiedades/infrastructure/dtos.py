26155a897121ca8c4726f06c9f1f3f319489be1a
Santiago-Cortes <Santiago.Cortes@coxautoinc.com>
2024-03-04 21:55:03 -0500

    feat(propiedad): capa infraesturcutra y seedwork para eventos

---
+++ src/propiedades/modules/propiedades/infrastructure/dtos.py
@@ -0,0 +1,35 @@
+import uuid
+
+from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy.orm import declarative_base, relationship
+
+from propiedades.config.db import db
+
+Base = db.declarative_base()
+
+
+class Fotografia(db.Model):
+    __tablename__ = "fotografias"
+    id = db.Column(db.String, primary_key=True)
+    fecha_creacion = db.Column(db.DateTime, nullable=False)
+    fecha_actualizacion = db.Column(db.DateTime, nullable=False)
+
+    nombre = db.Column(db.String, nullable=False)
+    descripcion = db.Column(db.String, nullable=False)
+
+    valor_contenido = db.Column(db.String, nullable=False)
+    tipo_contenido = db.Column(db.String, nullable=False)
+
+    propiedad_id = db.Column(db.String, db.ForeignKey("propiedades.id"))
+
+
+class Propiedad(db.Model):
+    __tablename__ = "propiedades"
+    id = db.Column(db.String, primary_key=True)
+    fecha_creacion = db.Column(db.DateTime, nullable=False)
+    fecha_actualizacion = db.Column(db.DateTime, nullable=False)
+
+    tipo_construccion = db.Column(db.String, nullable=False)
+    entidad = db.Column(db.String, nullable=False)
+
+    fotografias = db.relationship("Fotografia", backref="fotografias")
---
+++ src/propiedades/modules/propiedades/presentation/api.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request
+
+import propiedades.seedwork.presentation.api as api
+from propiedades.modules.propiedades.application.commands.create_propiedad import \
+    CreatePropiedadCommand
+from propiedades.modules.propiedades.application.mappers import \
+    PropiedadDTOJsonMapper
+from propiedades.modules.propiedades.application.queries.get_propiedad import \
+    GetPropiedadQuery
+from propiedades.modules.propiedades.application.queries.get_propiedades import \
+    GetPropiedadesQuery
+from propiedades.seedwork.application.commands import execute_command
+from propiedades.seedwork.application.queries import execute_query
+
+bp_prefix = "propiedades"
+bp: Blueprint = api.create_blueprint(bp_prefix, "/propiedades")
+
+
+@bp.route("/", methods=("POST",))
+def create():
+    propiedad_dict = request.json
+
+    map_propiedad = PropiedadDTOJsonMapper()
+    propiedad_dto = map_propiedad.external_to_dto(propiedad_dict)
+
+    comando = CreatePropiedadCommand(
+        id=propiedad_dto.id,
+        fecha_creacion=propiedad_dto.fecha_creacion,
+        tipo_construccion=propiedad_dto.tipo_construccion,
+        entidad=propiedad_dto.entidad, 
+        fotografias=propiedad_dto.fotografias
+    )
+    execute_command(comando)
+
+    return {}, 202
+
+
+@bp.route("/", methods=("GET",))
+@bp.route("/<id>", methods=("GET",))
+def retrieve(id=None):
+    map_propiedad = PropiedadDTOJsonMapper()
+    query_resultado = None
+    if not id:
+        query_resultado = execute_query(GetPropiedadesQuery())
+        query_resultado = [map_propiedad.dto_to_external(p) for p in query_resultado.result]
+    else:
+        query_resultado = execute_query(GetPropiedadQuery(id))
+        query_resultado = map_propiedad.dto_to_external(query_resultado.result)
+
+    return query_resultado
