09972c9f74681edfe25d3f46a1715ac0f678de22
s-cortes <s.cortes@uniandes.edu.co>
2024-03-04 22:03:09 -0500

    feat(propiedades): capa infraestructura

---
+++ src/propiedades/modules/propiedades/infrastructure/factories.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+from propiedades.modules.propiedades.domain.events import PropiedadCreada
+from propiedades.modules.propiedades.domain.repositories import \
+    PropiedadesRepository
+from propiedades.modules.propiedades.infrastructure.exceptions import \
+    InvalidRepositoryFactoryException
+from propiedades.modules.propiedades.infrastructure.repositories import \
+    PropiedadesRepositorySQLite
+from propiedades.modules.propiedades.infrastructure.schema.v1.mappers import \
+    EventoPropiedadCreadaMapper
+from propiedades.seedwork.domain.factories import Factory
+from propiedades.seedwork.domain.repositories import Mapper
+from propiedades.seedwork.infrastructure.schema.v1.mappers import \
+    IntegrationMapper
+
+
+@dataclass
+class RepositoryFactory(Factory):
+    def create(self, obj: type):
+        if obj == PropiedadesRepository.__class__:
+            return PropiedadesRepositorySQLite()
+        else:
+            raise InvalidRepositoryFactoryException
+
+
+class IntegrationMessageFactory(Factory):
+    def create(self, event: any) -> any:
+        if type(event) is PropiedadCreada:
+            mapper = EventoPropiedadCreadaMapper()
+            return mapper.external_to_message(event)
+        else:
+            print("[Propiedades] Error en IntegrationMessageFactory")
+            raise InvalidRepositoryFactoryException()
